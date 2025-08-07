bd3d4bc55cf2c30a6e8f83a01605c61b3656b7e4
Grady Link <github@grady.link>
2025-08-06 23:39:52 -0700

    fix: wii u support

--- Bitwise.cpp
+++ Bitwise.cpp
@@ -5,6 +5,12 @@
 #include <map>
 #include <string>
 
+#ifdef __WIIU__
+#include <coreinit/dynload.h>
+#endif
+
+extern "C" {
+
 std::any Bitwise_isNumberBits(std::map<std::string, std::any> arguments) {
   static const boost::regex bin_regex{"^-?[01]+$"};
   return boost::regex_match(anyToString(arguments["CENTRAL"]), bin_regex);
@@ -49,3 +55,17 @@ std::any Bitwise_bitwiseXor(std::map<std::string, std::any> arguments) {
 std::any Bitwise_bitwiseNot(std::map<std::string, std::any> arguments) {
   return ~anyToInt(arguments["CENTRAL"]);
 }
+}
+
+#ifdef __WIIU__
+int rpl_entry(OSDynLoad_Module module,
+              OSDynLoad_EntryReason reason) {
+  if (reason == OS_DYNLOAD_LOADED) {
+    // Do stuff on load
+  } else if (reason == OS_DYNLOAD_UNLOADED) {
+    // Do stuff on unload
+  }
+
+  return 0;
+}
+#endif
